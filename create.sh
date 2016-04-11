1b32d21a42c98764bd3c8072d358201f16dacca1
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-04-11 11:30:02 +0100

    Only use valid URL characters, to avoid having to encode it

--- create.sh
+++ create.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
 scope_url=$1
 
-output="report-$(date +%Y-%m-%d-%H:%M:%S).json"
+output="report-$(date +%Y-%m-%d-%H-%M-%S).json"
 
 curl --silent --location "${scope_url}/api/report" | jq . > "${output}"
 
 git add "${output}"
 git commit --message "Added \`${output}\`" --edit
 git push
+
+echo "https://raw.github.com/errordeveloper/scope-report-store/master/${output}"
